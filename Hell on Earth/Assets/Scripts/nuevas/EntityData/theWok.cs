4f88f20b393b142034d1fc2ee0204e57e6c005f6
AndreuRaga <andreurag@gmail.com>
2024-04-19 13:44:28 +0200

    Comentado un using de theWok

--- Hell on Earth/Assets/Scripts/nuevas/EntityData/theWok.cs
+++ Hell on Earth/Assets/Scripts/nuevas/EntityData/theWok.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Transactions;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
