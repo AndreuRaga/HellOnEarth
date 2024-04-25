c85d6c959a5afb8ce4301690489ecd17f35a6206
AndreuRaga <andreurag@gmail.com>
2024-04-25 19:50:26 +0200

    Creado script Wander

---
+++ Hell on Earth/Assets/Scripts/nuevas/EntityData/Wander.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Animator))]
+public class Wander : MonoBehaviour
+{
+    
+}
