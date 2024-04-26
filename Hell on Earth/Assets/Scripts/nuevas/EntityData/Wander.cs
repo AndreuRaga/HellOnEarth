d26bfb11ad93ca22fb203ccdca5c6b30c7fd024e
AndreuRaga <andreurag@gmail.com>
2024-04-26 12:35:23 +0200

    Movimiento del enemigo
    Hechos los idle y walk trees

--- Hell on Earth/Assets/Scripts/nuevas/EntityData/Wander.cs
+++ Hell on Earth/Assets/Scripts/nuevas/EntityData/Wander.cs
@@ -8,5 +8,56 @@ using Random = UnityEngine.Random;
 [RequireComponent(typeof(Animator))]
 public class Wander : MonoBehaviour
 {
-    
+    public float pursuitSpeed;
+    public float wanderSpeed;
+    float currentSpeed;
+
+    public float directionChangeInterval;
+    public bool followPlayer;
+    Coroutine moveCoroutine;
+
+    Rigidbody2D rb2d;
+    Animator animator;
+
+    Transform targetTransform = null;
+
+    Vector3 endPosition;
+    float currentAngle = 0;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentSpeed = wanderSpeed;
+        rb2d = GetComponent<Rigidbody2D>();
+        StartCoroutine(WanderRoutine());
+    }
+    IEnumerator WanderRoutine()
+    {
+        while (true)
+        {
+            ChooseNewEndpoint();
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            moveCoroutine = StartCoroutine(Move());
+            yield return new
+            WaitForSeconds(directionChangeInterval);
+        }
+    }
+    void ChooseNewEndpoint()
+    {
+        currentAngle += Random.Range(0, 360);
+        currentAngle = Mathf.Repeat(currentAngle, 360);
+        endPosition += Vector3FromAngle(currentAngle);
+    }
+    Vector3 Vector3FromAngle(float inputAngleDegrees)
+    {
+        float radians = inputAngleDegrees * Mathf.Deg2Rad;
+        return new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0);
+    }
+    IEnumerator Move()
+    {
+        return null; // TODO
+    }
 }
