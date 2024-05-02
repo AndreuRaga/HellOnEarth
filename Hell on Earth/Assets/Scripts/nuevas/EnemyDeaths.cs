90ce82c2d88532aa8458df5f21d51ad89cdb3361
AndreuRaga <andreurag@gmail.com>
2024-05-02 23:32:27 +0200

    Empezando porcentage de enemigos derrotados

---
+++ Hell on Earth/Assets/Scripts/nuevas/EnemyDeaths.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnemyDeaths : MonoBehaviour
+{
+    public TextMeshProUGUI percentage;
+    public int enemiesDead = 0;
+    public int enemyCount = 0;
+
+    void Start()
+    {
+        // Encontrar todos los GameObjects con la etiqueta "Enemy"
+        GameObject[] enemyList = GameObject.FindGameObjectsWithTag("Enemy");
+        enemyCount = enemyList.Length;
+        Debug.Log("Cantidad de enemigos: " + enemyCount);
+    }
+    void Update()
+    {
+        percentage.text = ((enemiesDead / enemyCount) * 100).ToString() + "%";
+    }
+}
