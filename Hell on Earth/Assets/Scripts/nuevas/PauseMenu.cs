b24295de77f5630bb47dac13c368539ee95dfdbf
AndreuRaga <andreurag@gmail.com>
2024-05-06 20:34:52 +0200

    OptionMenu prefab hecho

--- Hell on Earth/Assets/Scripts/nuevas/PauseMenu.cs
+++ Hell on Earth/Assets/Scripts/nuevas/PauseMenu.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject optionsMenu;
     public void OnGoToMainMenu()
     {
         GameManager.Instance.pause = false;
         GameManager.Instance.GoToMainMenu();
+    }
+    public void OnOptions()
+    {
+        if (optionsMenu != null)
+        {
+            optionsMenu.SetActive(!optionsMenu.activeSelf);
+        }
     }
     public void OnQuit()
     {
