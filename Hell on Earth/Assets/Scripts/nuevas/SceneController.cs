7284b807efb4900358f62ea1478792bef7e5013f
AndreuRaga <andreurag@gmail.com>
2024-05-07 19:57:42 +0200

    Música de los niveles hecha

--- Hell on Earth/Assets/Scripts/nuevas/SceneController.cs
+++ Hell on Earth/Assets/Scripts/nuevas/SceneController.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip firstLevelTheme;
+    public AudioClip secondLevelTheme;  
+    private const int FIRST_LEVEL_INDEX = 2;
+    private const int SECOND_LEVEL_INDEX = 3;
     void Start()
     {
+        if (firstLevelTheme != null && SceneManager.GetActiveScene().buildIndex == FIRST_LEVEL_INDEX)
+        {
+            GameManager.Instance.audioManager.PlayMusic(firstLevelTheme);
+        }
+        else if (secondLevelTheme != null && SceneManager.GetActiveScene().buildIndex == SECOND_LEVEL_INDEX)
+        {
+            GameManager.Instance.audioManager.PlayMusic(secondLevelTheme);
+        }
         GameManager.Instance.pause = false;
     }
 
