1e8e73677ec714eee0d01a81a445b50e79eca235
AndreuRaga <andreurag@gmail.com>
2024-05-10 21:32:13 +0200

    El audio de activación solo se escuchará una vez

--- Hell on Earth/Assets/Scripts/nuevas/Portal.cs
+++ Hell on Earth/Assets/Scripts/nuevas/Portal.cs
@@ -7,9 +7,11 @@ public class Portal : MonoBehaviour
     public GameObject portalArc;
     public LevelProgress levelProgress;
     public AudioClip portalActivated;
+    private bool audioPlayed;
 
     private void Start()
     {
+        audioPlayed = false;
         portalArc.SetActive(false);
     }
 
@@ -17,9 +19,10 @@ public class Portal : MonoBehaviour
     {
         if (levelProgress.percentageKilled >= 33)
         {
-            if (portalActivated != null)
+            if (portalActivated != null && !audioPlayed)
             {
                 GameManager.Instance.audioManager.PlaySound(portalActivated);
+                audioPlayed = true;
             }
             portalArc.SetActive(true);
         }
